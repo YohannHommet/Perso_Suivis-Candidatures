1cdb178f9eb25a6c9d1838421c0e35122fd7de38
YohannHommet <yohann.hommet@outlook.fr>
2021-07-08 22:36:42 +0200

    Code clean

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,12 @@
 const app = {
     navbar: document.querySelector('#nav'),
     pathname: window.location.pathname,
-    footer: document.querySelector('footer'),
 
-   init: () => {
-
+   init: () =>
+   {
        if (app.pathname !== '/') {
-           // app.navbar.classList.replace('navbar-dark', 'navbar-light');
            app.navbar.classList.replace('nav_transparent', 'nav_glass')
        }
-
-       let scene = document.getElementById('scene');
-       let parallaxInstance = new Parallax(scene, {
-           relativeInput: true,
-           clipRelativeInput: true,
-
-       });
-
    },
 
 }
