4b0c078fcc1b2ca6e3b055807320c510d2b2a6e7
YohannHommet <yohann.hommet@outlook.fr>
2021-07-16 10:39:40 +0200

    Adding relation between Applications and User

--- src/Entity/Applications.php
+++ src/Entity/Applications.php
@@ -66,6 +66,12 @@ class Applications
      */
     private ?string $remarques;
 
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="applications")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
+
     
     public function getId(): ?int
     {
@@ -177,6 +183,18 @@ class Applications
     public function setRemarques(?string $remarques): self
     {
         $this->remarques = $remarques;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
 
         return $this;
     }
